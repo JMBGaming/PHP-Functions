2efd00810f47c3eabc7f4618f92e19e06a6c25a4
Dilruwan Wijayasekara <dilruwan@gmail.com>
2012-11-02 02:09:07 +0000

    Initial Database Functions
    Useful functions for use with mysql.

---
+++ database.php
@@ -0,0 +1,43 @@
+<?php
+// Checks for mysql errors
+function mysql_prep($value) {
+	$magic_quotes_active = get_magic_quotes_gpc();
+	$new_enough_php = function_exists("mysql_real_escape_string"); // i.e PHP >= v4.3.0
+	
+	if ($new_enough_php) 
+	{
+		// undo any magic quote effects to mysql_real_escape_string can do the work
+		if($magic_quotes_active) 
+		{
+			$value = stripcslashes($value);
+		}
+		$value = mysql_real_escape_string($value);
+	} else { // Before PHP v4.3.0
+		// if magic quotes arn't areadly on then add slashes manually
+		if(!$magic_quotes_active)
+		{
+			$value = addslashes($value);
+		}
+		// if magic quotes are active, then slashes already exist.
+	}
+	return $value;
+}
+
+// Redirecting pages
+function redirect_to($location = NULL) {
+if($location != NULL) 
+	{
+	header("Location: {$location}");
+	exit;
+	}
+}
+
+// Checks the database query
+function confirm_query($result_set) 
+{
+	if (!$result_set) 
+	{
+		die("Database query failed: " . mysql_error());
+	}
+}
+?>
